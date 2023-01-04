7c9baf99dfab8cab94eef72772da00a083d62b77
Artur Duarte <arturduartemoraes@gmail.com>
2023-01-04 09:49:26 -0300

    Exercício 06 - Seção 04 - Finalizado

--- secao04_exercicio06/src/exercicio06/secao04/Exercicio.java
+++ secao04_exercicio06/src/exercicio06/secao04/Exercicio.java
@@ -1,5 +1,31 @@
 package exercicio06.secao04;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio {
 
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("*** LOJA DE PEÇAS ***");
+		System.out.println();
+		System.out.print("Insira o código, quantidade e valor da peça 1, nessa respectiva ordem: ");
+		float codigoPeca1 = sc.nextFloat();
+		float qntdePecas1 = sc.nextFloat();
+		float valorPeca1 = sc.nextFloat();
+
+		System.out.print("Insira o código, quantidade e valor da peça 2, nessa respectiva ordem: ");
+		float codigoPeca2 = sc.nextFloat();
+		float qntdePecas2 = sc.nextFloat();
+		float valorPeca2 = sc.nextFloat();
+
+		float total = qntdePecas1 * valorPeca1 + qntdePecas2 * valorPeca2;
+
+		System.out.println();
+
+		System.out.printf("VALOR A PAGAR: U$ %.2f", total);
+	}
 }
